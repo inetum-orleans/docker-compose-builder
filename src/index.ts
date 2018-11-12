7e56a3d9179031e826094ff3fb1bbf1be836b409
Rémi Alvergnat <remi.alvergnat@gfi.fr>
2018-11-12 10:45:04 +0100

    fix(package): Use flat barrels

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,6 @@
-import * as builderts from './builder'
-import * as configts from './config'
-import * as factoryts from './factory'
-import * as optionsts from './options'
-import * as serviceservicets from './service/service'
-import * as servicevolumets from './service/volume'
-import * as servicewithts from './service/with'
-import * as versionts from './version'
-export { builderts as builder }
-export { configts as config }
-export { factoryts as factory }
-export { optionsts as options }
-export const service = {
-  service: serviceservicets,
-  volume: servicevolumets,
-  with: servicewithts
-}
-export { versionts as version }
+export * from './builder'
+export * from './config'
+export * from './factory'
+export * from './options'
+export * from './version'
+export * from './service/index'
